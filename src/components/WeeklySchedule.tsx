b0ffe14def5056e6bc426131b85fb254cb863951
FedericoXPS <federico.frasca796@gmail.com>
2024-01-11 23:55:33 +0100

    ✨ feat(footer): add base footer with logo and weekly schedule

---
+++ src/components/WeeklySchedule.tsx
@@ -0,0 +1,55 @@
+import WavyLabel from './WavyLabel';
+import Badge from './ui/Badge';
+
+const schedule = [
+  {
+    label: 'Lunedì',
+    openAt: '07:00',
+    closeAt: '01:00'
+  },
+  {
+    label: 'Martedì'
+  },
+  {
+    label: 'Mercoledì',
+    openAt: '07:00',
+    closeAt: '01:00'
+  },
+  {
+    label: 'Giovedì',
+    openAt: '07:00',
+    closeAt: '01:00'
+  },
+  {
+    label: 'Venerdì',
+    openAt: '07:00',
+    closeAt: '01:00'
+  },
+  {
+    label: 'Sabato e Domenica',
+    openAt: '07:00',
+    closeAt: '01:00'
+  }
+];
+
+export default function WeeklySchedule() {
+  return (
+    <ul className='mt-8'>
+      {schedule.map((day, idx) => (
+        <li key={idx} className='mb-2'>
+          <WavyLabel
+            title={day.label}
+            subtitle={day.openAt && `dalle ${day.openAt} alle ${day.closeAt}`}
+          >
+            <Badge
+              color={day.openAt ? 'neutral' : 'primary'}
+              variant='contained'
+              label={day.openAt ? 'Aperti' : 'Chiusi'}
+              className={day.openAt ? 'text-primary' : ''}
+            />
+          </WavyLabel>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,3 +1,47 @@
+'use client';
+import WeeklySchedule from '@/components/WeeklySchedule';
+import Logo from '@/components/icons/Logo';
+import Button from '@/components/ui/Button';
+import { MapPinIcon } from '@heroicons/react/24/solid';
+
 export default function Footer() {
-  return <footer>Footer here</footer>;
+  return (
+    <footer className='top-wave relative bg-primary-0 pb-5 pt-4'>
+      <div className='container mx-auto'>
+        <section className='flex items-center gap-4'>
+          <Logo secondary className='w-[5.625rem] fill-primary' />
+          <div>
+            <div className='font-display text-3xl font-bold text-primary'>
+              Crema & Cioccolato
+            </div>
+            <span className='font-display text-2xl text-secondary-900'>
+              Barletta
+            </span>
+          </div>
+        </section>
+
+        <section>
+          <WeeklySchedule />
+        </section>
+
+        <section className='mt-16 flex items-center justify-between'>
+          <div className='max-w-52'>
+            <div className='font-serif text-2xl text-primary'>Ci trovi qui</div>
+            <p>Via Giacomo Piccininni, 62 Barletta (BT) +39 333 1234567</p>
+          </div>
+          <div>
+            <Button
+              onClick={() => console.log('test')}
+              fullWidth
+              variant='outlined'
+              startIcon={<MapPinIcon className='w-5' />}
+              className='bg-primary-0 drop-shadow-lg'
+            >
+              Mappa
+            </Button>
+          </div>
+        </section>
+      </div>
+    </footer>
+  );
 }
