e4cc56fc1e8de36f63f235b2bcf4bd9eddb788dc
FedericoXPS <federico.frasca796@gmail.com>
2024-04-07 19:32:48 +0200

    🐛 fix(fonts): fonts errors when imported as modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,29 @@
 import Footer from '@/components/layout/footer';
 import Header from '@/components/layout/header';
-import { dancingScript, nunitoSans, rhozaOne } from '@/utils/fonts';
 import type { Metadata } from 'next';
 import './globals.css';
+import { Dancing_Script, Nunito_Sans, Rozha_One } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: 'Crema e Cioccolato - Barletta',
   description: 'La gelateria dai gusti più folli e golosi di Barletta'
 };
+
+const nunitoSans = Nunito_Sans({
+  subsets: ['latin'],
+  variable: '--font-nunito-sans'
+});
+
+const rhozaOne = Rozha_One({
+  subsets: ['latin'],
+  variable: '--font-rhoza-one',
+  weight: ['400']
+});
+
+const dancingScript = Dancing_Script({
+  subsets: ['latin'],
+  variable: '--font-dancing-script'
+});
 
 export default function RootLayout({
   children
