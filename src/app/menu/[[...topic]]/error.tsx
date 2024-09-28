199d4955e2294048713605bde6bcb900405add74
FedericoXPS <federico.frasca796@gmail.com>
2024-09-28 23:55:48 +0200

    🐛 fix(menu error): customize menu error boundary

--- src/app/menu/[[...topic]]/error.tsx
+++ src/app/menu/[[...topic]]/error.tsx
@@ -12,11 +12,6 @@ export default function ErrorMenuByTopic({
 }) {
   return (
     <div className='container mx-auto min-h-[80dvh] items-center gap-16 pb-24 pt-10 md:grid md:grid-flow-col'>
-      <MeltedIcecream
-        className='relative -left-6 mb-10 hidden h-96 md:-top-20 md:left-0 md:mb-0 md:block md:h-[calc(100%+5rem)] md:w-full'
-        outlineClassName='fill-smokyBrown-800'
-        backgroundClassName='fill-secondary-0'
-      />
       <article className='prose'>
         <Logo
           secondary
@@ -26,9 +21,14 @@ export default function ErrorMenuByTopic({
           Oopsie! Qualcosa è andato storto
         </h1>
         <p className='font-sans'>{error.message}</p>
-        <Link type='button' buttonVariant='outlined' href='/' className='group'>
+        <Link
+          type='button'
+          buttonVariant='outlined'
+          href='/menu'
+          className='group'
+        >
           <Logo className='h-[60%] fill-primary group-hover:fill-secondary-0' />
-          Torna all&apos;homepage
+          Torna al menù
         </Link>
       </article>
     </div>
