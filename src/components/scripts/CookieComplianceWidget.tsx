2f3b52d1bc6212d55ee848d0d6fbbe67c4b18ecc
FedericoXPS <federico.frasca796@gmail.com>
2025-01-07 22:37:48 +0100

    ✨ feat(cookie): move iubendas variables in env

--- src/components/scripts/CookieComplianceWidget.tsx
+++ src/components/scripts/CookieComplianceWidget.tsx
@@ -6,19 +6,19 @@ export default function CookieComplianceWidget() {
       <Script id='cookie-compliance-widget' type='text/javascript'>
         {`
           var _iub = _iub || [];
-          _iub.csConfiguration = {"siteId":3886063,"cookiePolicyId":72419928,"lang":"it","storage":{"useSiteId":true}};
+          _iub.csConfiguration = {"siteId":${process.env.NEXT_PUBLIC_IUBENDA_SITE_ID},"cookiePolicyId":${process.env.NEXT_PUBLIC_IUBENDA_COOKIE_POLICY_ID},"lang":"it","storage":{"useSiteId":true}};
         `}
       </Script>
       <Script
         type='text/javascript'
-        src='https://cs.iubenda.com/autoblocking/3886063.js'
-      ></Script>
+        src={`https://cs.iubenda.com/autoblocking/${process.env.NEXT_PUBLIC_IUBENDA_SITE_ID}.js`}
+      />
       <Script
         type='text/javascript'
         src='//cdn.iubenda.com/cs/iubenda_cs.js'
         charSet='UTF-8'
         async
-      ></Script>
+      />
     </>
   );
 }
